6adcee27f448a160389709dc44d66c3fba92b27c
Dwire <gjdwyer8@gmail.com>
2021-01-02 01:42:12 -0500

    Updates

---
+++ precache-manifest.be195074a8244ae8e3fde4067c46445f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a114b2c36671441ab005740d77d2c80",
+    "url": "/fantasy-fitness-web/index.html"
+  },
+  {
+    "revision": "293397a2cd9cfc39812c",
+    "url": "/fantasy-fitness-web/static/css/main.8b74e557.chunk.css"
+  },
+  {
+    "revision": "09779f750143cf9607f5",
+    "url": "/fantasy-fitness-web/static/js/2.0c7f2225.chunk.js"
+  },
+  {
+    "revision": "07324541f92eab13ebe8f13b9791aaee",
+    "url": "/fantasy-fitness-web/static/js/2.0c7f2225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293397a2cd9cfc39812c",
+    "url": "/fantasy-fitness-web/static/js/main.8fceb287.chunk.js"
+  },
+  {
+    "revision": "39425e9865e9f29e8d04",
+    "url": "/fantasy-fitness-web/static/js/runtime-main.17e7491f.js"
+  }
+]);
