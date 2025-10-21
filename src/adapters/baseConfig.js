bdcee8359ca79f200c3faff66412786142864f54
Dwire <gjdwyer8@gmail.com>
2025-10-21 16:26:53 -0400

    new heroku backend deployment

--- src/adapters/baseConfig.js
+++ src/adapters/baseConfig.js
@@ -1,6 +1,7 @@
 
 // export const BASE_URL = 'http://localhost:3000/api/v1/'
-export const BASE_URL = 'https://winter-workout-challenge.herokuapp.com/api/v1/'
+// export const BASE_URL = 'https://winter-workout-challenge.herokuapp.com/api/v1/'
+export const BASE_URL = 'https://wwc-api-prod-995b417d3496.herokuapp.com/api/v1/'
 
 // export const config = (method, body=nil) => {
 //   return {
