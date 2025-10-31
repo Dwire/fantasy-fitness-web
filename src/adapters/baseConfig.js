51a30adfef59320d461494c43d6e132a57154da0
Dwire <gjdwyer8@gmail.com>
2025-10-31 11:47:28 -0400

    update baseConfig url

--- src/adapters/baseConfig.js
+++ src/adapters/baseConfig.js
@@ -1,6 +1,6 @@
 
-export const BASE_URL = 'http://localhost:3000/api/v1/'
-// export const BASE_URL = 'https://winter-workout-challenge-api-881a778a68cf.herokuapp.com/api/v1/' // STAGING
+// export const BASE_URL = 'http://localhost:3000/api/v1/'
+export const BASE_URL = 'https://winter-workout-challenge-api-881a778a68cf.herokuapp.com/api/v1/' // STAGING
 // export const BASE_URL = 'https://winter-workout-challenge.herokuapp.com/api/v1/'
 // export const BASE_URL = 'https://wwc-api-prod-995b417d3496.herokuapp.com/api/v1/'
 
